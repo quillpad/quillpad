a17ff2e5eb10ac532ca58f6089834396dab76806
Felix Fischer <fischerfelix06@gmail.com>
2022-11-18 18:54:56 +0100

    Revert "make sure navbar is colored correctly on more devices"
    This reverts commit 414cc6ca5e4e76a9df9c8fc5a192c474be37e3bf.

--- app/src/main/java/org/qosp/notes/ui/BaseActivity.kt
+++ app/src/main/java/org/qosp/notes/ui/BaseActivity.kt
@@ -4,14 +4,12 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import org.qosp.notes.R
 import org.qosp.notes.preferences.PreferenceRepository
 import org.qosp.notes.preferences.ThemeMode
 import javax.inject.Inject
@@ -46,31 +44,11 @@ open class BaseActivity : AppCompatActivity() {
             val isAutoDark =
                 themeMode == ThemeMode.SYSTEM.mode && (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
 
-            // Check which theme should be used (light, dark, black) and set navbar color accordingly
-            // if dark
             if (themeMode == ThemeMode.DARK.mode || isAutoDark) {
-                // if black
-                if (darkThemeModeStyle != null) {
+                darkThemeModeStyle?.let {
                     theme.applyStyle(darkThemeModeStyle, true)
-                    println("setting navbar to black")
-                    setNavbarColor(ContextCompat.getColor(this@BaseActivity, R.color.navBarBlack))
-                }
-                else {
-                    println("setting navbar to gray")
-                    setNavbarColor(ContextCompat.getColor(this@BaseActivity, R.color.navBarDark))
                 }
             }
-            // if light
-            else if (themeMode == ThemeMode.LIGHT.mode) {
-                println("setting navbar to white")
-                setNavbarColor(ContextCompat.getColor(this@BaseActivity, R.color.navBarLight))
-            }
-
         }
-    }
-    // Set navbar color on some devices
-    private fun setNavbarColor(color: Int) {
-        val window = this.window
-        window.navigationBarColor = color
     }
 }
