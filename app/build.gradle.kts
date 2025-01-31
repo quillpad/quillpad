f213a36e93ac1ea2dda929f35335afe78ccfed5f
Arumugam J <sixface@msn.com>
2025-01-31 14:30:08 -0800

    Convert to kts build script

---
+++ app/build.gradle.kts
@@ -0,0 +1,184 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlinParcelize)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.navigationSafeArgs)
+    alias(libs.plugins.hiltAndroid)
+}
+
+android {
+    compileSdk = 34
+    buildToolsVersion = "30.0.3"
+    namespace = "org.qosp.notes"
+
+    defaultConfig {
+        applicationId = "io.github.quillpad"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 30
+        versionName = "1.4.23"
+
+        testInstrumentationRunner = "org.qosp.notes.TestRunner"
+
+        // export schema
+        // https://stackoverflow.com/a/44645943/4594587
+        ksp {
+            arg("room.schemaLocation", "$projectDir/schemas")
+        }
+    }
+
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
+
+    if (project.hasProperty("keystore")) {
+        signingConfigs {
+            create("release") {
+                val keystoreFileArg = project.property("keystore").toString()
+                val storePassArg = project.property("storepass").toString()
+                val keyName = project.property("keyalias").toString()
+                val keypassArg = project.property("keypass").toString()
+                storeFile = file(keystoreFileArg)
+                storePassword = storePassArg
+                keyAlias = keyName
+                keyPassword = keypassArg
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isCrunchPngs = false
+            if (project.hasProperty("keystore")) {
+                signingConfig = signingConfigs.getByName("release")
+            }
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
+        }
+    }
+
+    flavorDimensions += "versions"
+    productFlavors {
+        create("googleFlavor") {
+            dimension = "versions"
+            buildConfigField("boolean", "IS_GOOGLE", "true")
+        }
+        create("defaultFlavor") {
+            dimension = "versions"
+            buildConfigField("boolean", "IS_GOOGLE", "false")
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    kapt {
+        javacOptions {
+            option("-Adagger.fastInit=ENABLED")
+            option("-Adagger.hilt.android.internal.disableAndroidSuperclassValidation=true")
+        }
+    }
+
+    sourceSets {
+        // Adds exported schema location as test app assets.
+        getByName("androidTest").assets.srcDirs(files("$projectDir/schemas"))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(libs.monitor)
+    implementation(libs.junit.ktx)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    coreLibraryDesugaring(libs.coreLibraryDesugaring)
+
+    implementation(libs.okhttp)
+    // AndroidX
+    implementation(libs.bundles.kotlin.androidX)
+
+    implementation(libs.bundles.kotlin.deps)
+
+    // Material Components
+    implementation(libs.material)
+
+    // Room
+    ksp(libs.roomCompiler)
+    implementation(libs.roomRuntime)
+    implementation(libs.roomKtx)
+    testImplementation(libs.roomTesting)
+    androidTestImplementation(libs.roomTesting)
+
+    // Lifecycle
+    implementation(libs.bundles.kotlin.lifecycle)
+
+    // Security
+    implementation(libs.securityCrypto)
+
+    // Flow Preferences
+    implementation(libs.flowPreferences)
+
+    // DataStore Preferences
+    implementation(libs.datastorePreferences)
+    implementation(libs.datastoreext)
+
+    // Markwon
+    implementation(libs.bundles.markwon)
+
+    // Work Manager
+    implementation(libs.workRuntimeKtx)
+    androidTestImplementation(libs.workTesting)
+
+    // Coil
+    implementation(libs.coil)
+    implementation(libs.coilVideo)
+    implementation(libs.coilGif)
+
+    // PhotoView
+    implementation(libs.photoview)
+
+    // Hilt
+    implementation(libs.androidxHiltWork)
+    implementation(libs.hiltAndroid)
+    androidTestImplementation(libs.hiltAndroidTesting)
+    kaptAndroidTest(libs.hiltAndroidCompiler)
+    kapt(libs.hiltCompiler)
+    kapt(libs.androidxHiltCompiler)
+
+    // ExoPlayer
+    implementation(libs.exoplayerCore)
+    implementation(libs.exoplayerUi)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2Convertor)
+
+    // LeakCanary
+    debugImplementation(libs.leakcanaryAndroid)
+}
