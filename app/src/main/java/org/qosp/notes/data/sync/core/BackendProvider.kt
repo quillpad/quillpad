dd2c21d7cbd17175dae399ee4f1137054f98916d
Arumugam J <sixface@msn.com>
2025-09-12 16:20:24 -0700

    Introduce support for trusting self-signed certificates in Nextcloud integration

--- app/src/main/java/org/qosp/notes/data/sync/core/BackendProvider.kt
+++ app/src/main/java/org/qosp/notes/data/sync/core/BackendProvider.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import org.qosp.notes.data.sync.fs.StorageBackend
 import org.qosp.notes.data.sync.fs.StorageConfig
-import org.qosp.notes.data.sync.nextcloud.NextcloudAPI
+import org.qosp.notes.data.sync.nextcloud.NextcloudAPIProvider
 import org.qosp.notes.data.sync.nextcloud.NextcloudBackend
 import org.qosp.notes.data.sync.nextcloud.NextcloudConfig
 import org.qosp.notes.di.SyncScope
@@ -20,7 +20,7 @@ import org.qosp.notes.ui.utils.ConnectionManager
 
 class BackendProvider(
     private val context: Context,
-    private val nextcloudApi: NextcloudAPI,
+    private val nextcloudApiProvider: NextcloudAPIProvider,
     preferenceRepository: PreferenceRepository,
     syncingScope: SyncScope,
     private val connectionManager: ConnectionManager,
@@ -36,7 +36,7 @@ class BackendProvider(
     ) { service, nextcloudConfig, storageConfig ->
         when (service) {
             CloudService.DISABLED -> null
-            CloudService.NEXTCLOUD -> nextcloudConfig?.let { NextcloudBackend(nextcloudApi, it) }
+            CloudService.NEXTCLOUD -> nextcloudConfig?.let { NextcloudBackend(nextcloudApiProvider, it) }
             CloudService.FILE_STORAGE -> storageConfig?.let { StorageBackend(context, it) }
         }
     }.stateIn(syncingScope, SharingStarted.Companion.Eagerly, null)
@@ -46,5 +46,4 @@ class BackendProvider(
             syncMode = pref.value?.syncMode,
             cloudService = syncProvider.value?.type
         )
-
 }
