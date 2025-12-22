acfcbeb427635a1464b72042f85f853a4112b257
Arumugam J <sixface@msn.com>
2025-12-22 08:51:11 -0800

    Handle notes getting moved to Trash
    - Update `getAll()` method to allow nullable results across sync backends for better error handling.
    - Add logic to reset ID mappings when Nextcloud URL or username changes to ensure consistency.

--- app/src/main/java/org/qosp/notes/ui/sync/nextcloud/NextcloudViewModel.kt
+++ app/src/main/java/org/qosp/notes/ui/sync/nextcloud/NextcloudViewModel.kt
@@ -7,14 +7,17 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.qosp.notes.data.repo.NoteRepository
 import org.qosp.notes.data.sync.nextcloud.BackendValidationResult
 import org.qosp.notes.data.sync.nextcloud.NextcloudConfig
 import org.qosp.notes.data.sync.nextcloud.ValidateNextcloud
+import org.qosp.notes.preferences.CloudService
 import org.qosp.notes.preferences.PreferenceRepository
 
 class NextcloudViewModel(
     private val preferenceRepository: PreferenceRepository,
     private val validateNextcloud: ValidateNextcloud,
+    private val noteRepository: NoteRepository
 ) : ViewModel() {
 
     val username = preferenceRepository.getEncryptedString(PreferenceRepository.NEXTCLOUD_USERNAME)
@@ -24,6 +27,12 @@ class NextcloudViewModel(
         if (!URLUtil.isHttpsUrl(url)) return@launch
 
         val url = if (url.endsWith("/")) url else "$url/"
+        val previousUrl = preferenceRepository.getEncryptedString(PreferenceRepository.NEXTCLOUD_INSTANCE_URL).first()
+
+        if (url != previousUrl) {
+            noteRepository.deleteIdMappingsForCloudService(CloudService.NEXTCLOUD)
+        }
+
         preferenceRepository.putEncryptedStrings(
             PreferenceRepository.NEXTCLOUD_INSTANCE_URL to url,
         )
@@ -38,6 +47,11 @@ class NextcloudViewModel(
 
         val response = withContext(Dispatchers.IO) { validateNextcloud(config) }
         if (response == BackendValidationResult.Success) {
+            val previousUsername =
+                preferenceRepository.getEncryptedString(PreferenceRepository.NEXTCLOUD_USERNAME).first()
+            if (username != previousUsername) {
+                noteRepository.deleteIdMappingsForCloudService(CloudService.NEXTCLOUD)
+            }
             preferenceRepository.putEncryptedStrings(
                 PreferenceRepository.NEXTCLOUD_USERNAME to username,
                 PreferenceRepository.NEXTCLOUD_PASSWORD to password,
